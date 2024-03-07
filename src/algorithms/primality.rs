2a7c0713cd766e4f4f62b731724eae5d5c3d0e8d
Abdoulaye Ba <abba@f0r4s17.codam.nl>
2024-03-07 15:55:30 +0100

    remove big_int

--- src/algorithms/primality.rs
+++ src/algorithms/primality.rs
@@ -1,7 +1,7 @@
-use num::{One, Zero};
+use num::{One, Zero, BigInt};
 use rand::Rng;
 use modpow::*;
-use num_bigint::BigInt;
+// use num_bigint::BigInt;
 
 pub fn is_prime(n: u64, k: usize) -> bool {
     if k > 100 {
--- src/commands/genrsa.rs
+++ src/commands/genrsa.rs
@@ -1,5 +1,5 @@
-use num::One;
-use num_bigint::BigInt;
+use num::{One, BigInt};
+// use num_bigint::c;
 
 use crate::generate_prime;
 use crate::algorithms::primality::{lcm, mod_inverse};
